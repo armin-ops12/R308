1af4da7c9cc52eb04c57430c2e5169db72b65af0
armin-ops12 <arminhodzic127@gmail.com>
2026-09-11 16:19:45 +0200

    maj

--- main.py
+++ main.py
@@ -1,16 +1,96 @@
-# This is a sample Python script.
+import math
+class Point:
+    """
+    Représente un point dans un repère cartésien 2D.
+    Attributes:
+        x (float): L'abscisse du point.
+        y (float): L'ordonnée du point.
+    """
+    def __init__(self, x: float = 0.0, y: float = 0.0) -> None:
+        """
+        :param x: abscisse du point (0.0 par défaut)
+        :param y: ordonnée du point (0.0 par défaut)
+        """
+        self.__x = float(x)
+        self.__y = float(y)
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+    def distanceCoord(self,a:float,b:float)->float:
+        """
+        :param a: abscisse de l'autre point
+        :param b: ordonnée de l'autre point
+        :return: distance entre les 2 points
+        """
+        return math.sqrt((self.__x - a) ** 2 + (self.__y - b) ** 2)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def distancePoint(self, camarade: "Point") -> float:
+        """:
+        param camarade: l'autre objet Point
+        :return: distance entre les 2 points
+        """
+        return self.distanceCoord(camarade.__x, camarade.__y)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    def __str__(self) -> str:
+        return f"Point : ({self.__x},{self.__y})"
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+class Cercle:
+    """Classe représentant un cercle[cite: 1]."""
+
+    def __init__(self, rayon: float, centre: Point=Point(0,0)):
+        """Constructeur gérant l'origine par défaut ou un centre spécifié."""
+        self.__rayon = float(rayon)
+        self.__centre = centre
+
+
+    def diametre(self) -> float:
+        """Calcule le diamètre du cercle."""
+        return 2 * self.__rayon
+
+    def perimetre(self) -> float:
+        """ Calcule le périmètre du cercle."""
+        return 2 * math.pi * self.__rayon
+
+    def surface(self) -> float:
+        """Calcule la surface du cercle."""
+        return math.pi * (self.__rayon**2)
+
+
+    def est_en_intersection(self, autre: "Cercle") -> bool:
+        """Vérifie l'intersection avec un autre cercle."""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    def contient_point(self, p: Point) -> bool:
+        """(5) Vérifie si un Point A fait partie du cercle (disque)[cite: 1]."""
+        return self.centre.distancePoint(p) <= self.rayon
+
+
+
+if __name__ == "__main__":
+   point1 = Point(2,3.4)
+   print(point1)
+   point2 = Point(2,5)
+   print(point2)
+   print(point1.distancePoint(point2))
